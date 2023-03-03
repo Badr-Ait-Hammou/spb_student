ddb0bea641faf2e016358bea93856591125591f1
badr <badraithammou2000@gmail.com>
2023-03-03 20:20:00 +0100

    test577887

--- src/main/java/com/example/first_project/service/StudentService.java
+++ src/main/java/com/example/first_project/service/StudentService.java
@@ -27,7 +27,7 @@ public class StudentService {
             return -1;
         }else{
             studentRepository.save(student);
-            return 40;
+            return 4;
         }
 
     }
